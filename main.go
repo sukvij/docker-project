63468f5e19558d99311bf3c4138a8dd76bdd8ae9
vijendra sukariya <vijju8769310002@gmail.com>
2023-12-29 13:16:53 +0530

    16th commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,15 +12,24 @@ type User struct {
 	Name string
 }
 
-var users []*User
+var users map[string]*User
 
 func main() {
 	router := gin.Default()
-	users = append(users, &User{Id: "1", Name: "vijendra"})
+	users = make(map[string]*User)
+	users["1"] = &User{Id: "1", Name: "vijendra"}
+	users["2"] = &User{Id: "2", Name: "sukariya"}
 	router.GET("/", getAllUsers)
+	router.GET("/{id:id}", getUserById)
 	router.Run(":8000")
 }
 
 func getAllUsers(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, users)
+}
+
+func getUserById(ctx *gin.Context) {
+	id := ctx.Param("id")
+	fmt.Println(id)
+	ctx.JSON(http.StatusOK, users[id])
 }
